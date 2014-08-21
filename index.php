3ba92a3ea34e0dcf9c47e5e93e4ecf3f5e850c70
AlexCoderbit <sashayushchak@gmail.com>
2014-08-21 14:36:41 +0300

    start

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>new template</title>
+	<meta name='author' content='CoderStudio, info@coderstudio.net'>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<h1>Hello, World!</h1>
+</body>
+</html>
